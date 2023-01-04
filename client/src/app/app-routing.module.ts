7a038e59ebc0b30f641e7387c53153783c4d8492
SunnySun0813 <SASunny0813@gmail.com>
2023-01-04 14:46:44 -0800

    set up routing

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,38 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EmployeeComponent } from './features/employee/employee.component';
+import { HousingComponent } from './features/employee/housing/housing.component';
+import { OnboardingApplicationComponent } from './features/employee/onboarding-application/onboarding-application.component';
+import { PersonalInformationComponent } from './features/employee/personal-information/personal-information.component';
+import { VisaStatusComponent } from './features/employee/visa-status/visa-status.component';
+import { EmployeeProfilesComponent } from './features/hr/employee-profiles/employee-profiles.component';
+import { HiringManagementComponent } from './features/hr/hiring-management/hiring-management.component';
+import { HousingManagementComponent } from './features/hr/housing-management/housing-management.component';
+import { HrComponent } from './features/hr/hr.component';
+import { VisaManagementComponent } from './features/hr/visa-management/visa-management.component';
+import { SigninComponent } from './features/signin/signin.component';
+import { SignupComponent } from './features/signup/signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'signin', component: SigninComponent},
+  { path:'signup/:regtoken', component: SignupComponent},
+  { path:'employee', component: EmployeeComponent,
+    children:[
+      {path:'onboarding-application', component: OnboardingApplicationComponent},
+      {path:'personal-information', component: PersonalInformationComponent},
+      {path:'visa-status', component: VisaStatusComponent},
+      {path:'housing', component: HousingComponent},
+    ]
+  },
+  { path:'hr', component: HrComponent,
+    children:[
+      {path:'employee-profiles', component: EmployeeProfilesComponent},
+      {path:'visa-mangement', component: VisaManagementComponent},
+      {path:'hiring-mangement', component: HiringManagementComponent},
+      {path:'housing-mangement', component: HousingManagementComponent},
+    ]
+  },
+  { path:'**', redirectTo:'/first' }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ client/src/app/features/employee/visa-status/visa-status.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-visa-status',
+  templateUrl: './visa-status.component.html',
+  styleUrls: ['./visa-status.component.scss']
+})
+export class VisaStatusComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
