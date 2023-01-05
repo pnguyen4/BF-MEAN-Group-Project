cfd4ff22c286a5a1daf90ee5409e28ede1cb863c
Phillip Nguyen <pnguyen4711@gmail.com>
2023-01-05 12:18:27 -0500

    start of registration token and link emailing

---
+++ server/controllers/Registration.controller.js
@@ -0,0 +1,21 @@
+const path = require('path');
+require("dotenv").config({ path: path.join(__dirname, '../.env') });
+const nodemailer = require('nodemailer');
+
+const { RegToken } = require ('../models/RegToken.model');
+
+exports.getRegTokens = async (req, res) => {
+  try {
+
+  } catch (error) {
+
+  }
+};
+
+exports.createRegToken = async (req, res) => {
+  try {
+
+  } catch (error) {
+
+  }
+}
---
+++ server/routes/RegistrationRouter.js
@@ -0,0 +1,8 @@
+const router = require("express").Router();
+const controller = require("../controllers/Registration.controller.js");
+
+// TODO: add auth middleware to check if user is signed in AND is HR admin
+//router.get('/api/regtokens', controller.getRegTokens);
+
+// TODO: add auth middleware to check if user is signed in AND is HR admin
+//router.post('/api/regtokens', controller.createRegToken);
