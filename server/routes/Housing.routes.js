ba67d267cb8ad5af01daafb85da6334331b60804
Phillip Nguyen <pnguyen4711@gmail.com>
2023-01-07 02:50:44 -0500

    middleware no work, fix later

--- server/routes/Housing.routes.js
+++ server/routes/Housing.routes.js
@@ -3,7 +3,7 @@ const router = require("express").Router();
 const { verifyUser, verifyEmployee, verifyHr } = require("../middleware/auth");
 
 // TODO: add respective middlewares, first need to add http interceptor on client to add token to header
-router.get('/api/housing/:id', verifyUser, controller.getHousingDetails);
+router.get('/api/housing/:id', controller.getHousingDetails);
 router.get('/api/housing', controller.getHousingSummary);
 router.post('/api/housing', controller.createHousing);
 router.delete('/api/housing/:id', controller.deleteHousing);
