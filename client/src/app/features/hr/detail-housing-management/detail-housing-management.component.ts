e6c0c09578661f48d6dcdfea6d2cb9bf3366b9f1
Phillip Nguyen <pnguyen4711@gmail.com>
2023-01-08 00:16:41 -0500

    cleanup, hr housing details page

--- client/src/app/features/hr/detail-housing-management/detail-housing-management.component.ts
+++ client/src/app/features/hr/detail-housing-management/detail-housing-management.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { HousingService } from 'app/shared/housing.service';
+import { Store } from '@ngrx/store';
+import { EmployeeHousingAction } from '../../../store/housing.action';
+import { selectEmployeeHousing, selectFacReports } from '../../../store/housing.selector';
+import { ActivatedRoute } from '@angular/router';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-detail-housing-management',
@@ -7,9 +13,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailHousingManagementComponent implements OnInit {
 
-  constructor() { }
+  housing$ = this.store.select(selectEmployeeHousing);
+  facReports$ = this.store.select(selectFacReports);
+  houseid: string = 'default';
+
+  constructor(private housingService: HousingService,
+              private _activatedRoute: ActivatedRoute,
+              private store: Store) { }
 
   ngOnInit(): void {
+    this._activatedRoute.params.subscribe(params => {
+      this.houseid = params['housingid'];
+    });
+
+    this.housingService.getHousingDetails(this.houseid).subscribe(res => {
+      this.store.dispatch(EmployeeHousingAction.loadHousing({house: res.house}));
+    });
+
+    this.housingService.getHouseFacilityReports(this.houseid).subscribe(res => {
+      this.store.dispatch(EmployeeHousingAction.loadFacilityReports({
+        facReports: res.facReports
+      }));
+    });
   }
 
 }
