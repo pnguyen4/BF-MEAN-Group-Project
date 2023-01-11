79cfadf6b5d19764b644e19fc9fd80f433ed1ee0
Phillip Nguyen <pnguyen4711@gmail.com>
2023-01-11 01:56:25 -0500

    protect more routes

--- server/routes/Housing.routes.js
+++ server/routes/Housing.routes.js
@@ -1,18 +1,18 @@
 const controller = require("../controllers/Housing.controller");
 const router = require("express").Router();
-const { verifyUser, verifyEmployee, verifyHr } = require("../middleware/auth");
+const { verifyToken, verifyEmployee, verifyHr } = require("../middleware/auth");
 
 // TODO: add respective middlewares, first need to add http interceptor on client to add token to header
-router.get('/api/housing/:id', controller.getHousingDetails);
-router.get('/api/housing', controller.getHousingSummary);
-router.post('/api/housing', controller.createHousing);
-router.delete('/api/housing/:id', controller.deleteHousing);
+router.get('/api/housing/:id', [verifyToken], controller.getHousingDetails);
+router.get('/api/housing', [verifyToken], controller.getHousingSummary);
+router.post('/api/housing', [verifyToken], controller.createHousing);
+router.delete('/api/housing/:id', [verifyToken], controller.deleteHousing);
 
-router.get('/api/housing/:id/reports', controller.getHouseFacilityReports);
-router.post('/api/housing/:id/reports', controller.createFacilityReport);
+router.get('/api/housing/:id/reports', [verifyToken], controller.getHouseFacilityReports);
+router.post('/api/housing/:id/reports', [verifyToken], controller.createFacilityReport);
 
-router.get('/api/housing/:houseid/reports/:reportid', controller.getOneFacReport);
-router.post('/api/housing/:houseid/reports/:reportid/msg', controller.addMsgToFacilityReport);
-router.put('/api/housing/:houseid/reports/:reportid/msg/:msgid', controller.editMsgOnFacilityReport);
+router.get('/api/housing/:houseid/reports/:reportid', [verifyToken], controller.getOneFacReport);
+router.post('/api/housing/:houseid/reports/:reportid/msg', [verifyToken], controller.addMsgToFacilityReport);
+router.put('/api/housing/:houseid/reports/:reportid/msg/:msgid', [verifyToken], controller.editMsgOnFacilityReport);
 
 module.exports = router;
